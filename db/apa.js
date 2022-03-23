4e48d000a7739b261f91f80612ddb666f8bc3aa0
Fernando N. Marco <97688079+fnma88@users.noreply.github.com>
2022-03-23 19:42:44 -0300

    productos en la carpeta db

---
+++ db/apa.js
@@ -0,0 +1,11 @@
+const apa = [
+  {
+    name: "A TROCHE Y MOCHE",
+    price: 4.5,
+    category: "APA",
+    sizecc: 473,
+    ibus: "5.2%",
+    description:
+      "Seguimos probando los lúpulos frescos desde Nueva Zelanda y en esta ocasión elegimos Moutere y Riwaka. Fuerte aroma y sabor a pomelo y naranja y algo de pino resinoso por parte del Moutere.",
+  },
+];
---
+++ db/lager.js
@@ -0,0 +1,47 @@
+const lager = [
+  {
+    name: "HUÉ LITE",
+    price: 4.5,
+    category: "LAGER",
+    sizecc: 355,
+    ibus: "4.0%",
+    description:
+      "Nuestra cerveza para todas las ocasiones. Esta lager baja en calorías es ideal para todos los gustos, suave, limpia y refrescante.",
+  },
+  {
+    name: "GUIDAÍ",
+    price: 4.5,
+    category: "LAGER",
+    sizecc: 473,
+    ibus: "5.0%",
+    description:
+      "Cerveza lager de color rojo profundo con notas sutiles a caramelo y galletas tostadas debido a las maltas especiales que utilizamos. Limpia, refrescante y maltosa. El nombre proviene del idioma Charrúa. Guidaí = Luna.",
+  },
+  {
+    name: "GUIDAÍ",
+    price: 4.5,
+    category: "LAGER",
+    sizecc: 473,
+    ibus: "4.5%",
+    description:
+      "Cerveza lager sin filtrar madurada por 5 semanas a 0°C. Usamos los ingredientes indicados para producir una Pilsner de estilo Checo, incluyendo lúpulos tradicionales Saaz (provenientes de República Checa). Una cerveza limpia y refrescante con leves notas a galleta, miel y pimienta proveniente del lúpulo. El nombre proviene del idioma Charrúa. Hué = Agua.",
+  },
+  {
+    name: "LA DÓCIL",
+    price: 4.5,
+    category: "LAGER",
+    sizecc: 473,
+    ibus: "4.5%",
+    description:
+      "Ligera y refrescante como nuestra Hué Lite, pero con todo el sabor y aroma de una NE Ipa. Light lager con dry hop de lúpulo Citra.",
+  },
+  {
+    name: "ZAPICÁN",
+    price: 4.5,
+    category: "LAGER",
+    sizecc: 473,
+    ibus: "6.0%",
+    description:
+      "Usamos lúpulos Cascade y Amarillo sobre una base de malta Maris Otter y un toque de malta caramelo. Es de amargor medio-alto y final seco con aroma a pino y cítricos. El nombre nace en honor al épico cacique charrúa Zapicán, líder de la batalla de San Gabriel en 1573. Uno de los primeros uruguayos que defendió estas tierras.",
+  },
+];

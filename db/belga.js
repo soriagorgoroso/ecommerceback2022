6a33dd6dc7c7f4bce073bbe125b9ad517ce8d90f
Fernando N. Marco <97688079+fnma88@users.noreply.github.com>
2022-03-23 19:47:16 -0300

    Create belga.js

---
+++ db/belga.js
@@ -0,0 +1,11 @@
+const belga = [
+  {
+    name: "CRUZADA",
+    price: 4.5,
+    category: "BELGA",
+    sizecc: 473,
+    ibus: "9.0%",
+    description:
+      "Una cerveza tipo belga de color ámbar profundo a marrón cobrizo. Aromas y sabores caramelizados, dulzones y a malta. La levadura belga tambien otorga esteres frutales. De cuerpo medio-alto con cierta tibieza por el alcohol, que junto a una alta carbonatación crea una espuma cremosa y persistente.",
+  },
+];

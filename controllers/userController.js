872b1044fe36c46dc9b711b4492570af90ed91e8
Fernando N. Marco <97688079+fnma88@users.noreply.github.com>
2022-03-24 15:10:51 -0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,55 @@
 const User = require("../models/User");
 
 // Display a listing of the resource.
-async function index(req, res) {}
+async function index(req, res) {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(400).json({ error: "error", msg: "users not found" });
+  }
+}
 
 // Display the specified resource.
-async function show(req, res) {}
+async function show(req, res) {
+  try {
+    const user = await User.findOne({
+      username: req.params.username,
+    });
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ error: "error", msg: "user not found" });
+  }
+}
 
 // Show the form for creating a new resource
-async function create(req, res) {}
+//async function create(req, res) {}
 
 // Store a newly created resource in storage.
-async function store(req, res) {}
+async function store(req, res) {
+  try {
+    const userNameExists = await User.findOne({ username: req.body.username });
+    if (userNameExists) {
+      res.status(409).json({ msg: "Username already in use, pick another one." });
+    } else {
+      const user = await User.create({
+        ...req.body,
+      });
+      res.json({ user, msg: "User created succesfully" });
+    }
+  } catch (error) {
+    res.status(400).json("error");
+  }
+}
 
 // Show the form for editing the specified resource.
-async function edit(req, res) {}
+//async function edit(req, res) {}
 
 // Update the specified resource in storage.
 async function update(req, res) {}
 
 // Remove the specified resource from storage.
-async function destroy(req, res) {}
+//async function destroy(req, res) {}
 
 // Otros handlers...
 // ...
@@ -27,9 +57,9 @@ async function destroy(req, res) {}
 module.exports = {
   index,
   show,
-  create,
+  //create,
   store,
-  edit,
+  //edit,
   update,
-  destroy,
+  //destroy,
 };

28d7707deb598b9f150820908d67174bac78b48d
soriasebastian <soriagorgoroso@gmail.com>
2022-03-24 15:43:55 -0300

    Route actua

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -5,18 +5,18 @@ const checkJwt = require("express-jwt");
 const tokenExist = require("../middlewares/tokenExist");
 
 //*****    Ruta para home *************** */
-categoryRouter.get("/categorys", categoryController.index);
+categoryRouter.get("/categories", categoryController.index);
 //******    Ruta para obtener una category************ */
-categoryRouter.get("/categorys/:id", categoryController.show);
+categoryRouter.get("/categories/:id", categoryController.show);
 //******    Midlleware para rutas privadas ************ */
 categoryRouter.use(checkJwt({ secret: process.env.ACCESS_TOKEN_SECRET, algorithms: ["HS256"] }));
 //******    Ruta obtener category ************ */
 categoryRouter.use(tokenExist);
 //******    Ruta crear category ************ */
-categoryRouter.post("/categorys", categoryController.create);
+categoryRouter.post("/categories", categoryController.create);
 //******    Ruta editar category ************ */
-categoryRouter.patch("/categorys-:id", categoryController.update);
+categoryRouter.patch("/categories:id", categoryController.update);
 //*****   Ruta delete tweet ************* */
-categoryRouter.delete("/categorys/:id", categoryController.destroy);
+categoryRouter.delete("/categories/:id", categoryController.destroy);
 
 module.exports = categoryRouter;

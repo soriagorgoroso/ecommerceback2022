04b4f834a6d05877377f18f3270f5b87eca0f76f
Fernando N. Marco <97688079+fnma88@users.noreply.github.com>
2022-03-29 15:21:49 -0300

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,13 +2,16 @@ const express = require("express");
 const orderRouter = express.Router();
 const { index, show, create, update, destroy } = require("../controllers/orderController");
 const checkJwt = require("express-jwt");
-const tokenExist = require("../middlewares/tokenExist");
+//const tokenExist = require("../middlewares/tokenExist");
+const isAdmin = require("../middlewares/isAdmin.js");
 
 orderRouter.get("/", index);
 orderRouter.get("/:id", show);
 orderRouter.use(checkJwt({ secret: process.env.ACCESS_TOKEN_SECRET, algorithms: ["HS256"] }));
 //******    Midlleware para rutas privadas ************ */
 // //******    Ruta obtener order ************ */
+orderRouter.use(isAdmin);
+
 orderRouter.post("/", create);
 orderRouter.patch("/:id", update);
 orderRouter.delete("/:id", destroy);

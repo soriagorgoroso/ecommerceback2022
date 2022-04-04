6659f4c0ddd5e9fe8b45c480967793edc4ac77f0
soriasebastian <soriagorgoroso@gmail.com>
2022-04-04 15:20:42 -0300

    User create a pulbic

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,13 +5,12 @@ const checkJwt = require("express-jwt");
 //const tokenExist = require("../middlewares/tokenExist");
 const isAdmin = require("../middlewares/isAdmin.js");
 
+// Store a newly created resource in storage.
+userRouter.post("/", userController.store);
 //******    Midlleware para rutas privadas ************ */
 userRouter.use(checkJwt({ secret: process.env.ACCESS_TOKEN_SECRET, algorithms: ["HS256"] }));
 //******    Ruta obtener user ************ */
 userRouter.use(isAdmin);
-
-// Store a newly created resource in storage.
-userRouter.post("/", userController.store);
 
 // Display a listing of the resource.
 userRouter.get("/", userController.index);
